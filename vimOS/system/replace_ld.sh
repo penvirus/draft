68607e3d6cbdf39b0c208f881cdefc66b68d4cf4
Tzung-Bi Shih <penvirus@gmail.com>
2013-11-20 14:50:51 +0800

    developing

---
+++ vimOS/system/replace_ld.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+root=$1
+
+for file in $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
+do
+	cp -uv $file{,.orig}
+	sed -e 's@/lib\(64\)\?\(32\)\?/ld@'$root'/usr/bin/ld@g' $file.orig > $file
+		#-e 's@/usr@'$root'/usr@g' $file.orig > $file
+	echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "'$root'/usr/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+	touch $file.orig
+done
