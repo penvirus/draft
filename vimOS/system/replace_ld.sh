e13beb55d7c4c6898973fdaa71e4522a0b679e83
Tzung-Bi Shih <penvirus@gmail.com>
2013-12-12 16:22:19 +0800

    temp tools

--- vimOS/system/replace_ld.sh
+++ vimOS/system/replace_ld.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 root=$1
+root=/tools
 
 for file in $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
 do
@@ -8,10 +9,10 @@ do
 	sed -e 's@/lib\(64\)\?\(32\)\?/ld@'$root'&@g' \
 		-e 's@/usr@'$root'/usr@g' $file.orig > $file
 #define STANDARD_STARTFILE_PREFIX_1 "'$root'/usr/lib64/"
-	#echo '
+	echo '
 #undef STANDARD_STARTFILE_PREFIX_1
 #undef STANDARD_STARTFILE_PREFIX_2
-#define STANDARD_STARTFILE_PREFIX_1 "/usr/lib64/"
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib64/"
 #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
 	touch $file.orig
 done
