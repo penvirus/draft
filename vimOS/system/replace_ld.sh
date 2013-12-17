0298e5d2f4af6eb00dcee511bd80c9cd00716068
Tzung-Bi Shih <penvirus@gmail.com>
2013-12-17 11:33:36 +0800

    developing

--- vimOS/system/replace_ld.sh
+++ vimOS/system/replace_ld.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 root=$1
-root=/tools
 
 for file in $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
 do
 	cp -uv $file{,.orig}
 	sed -e 's@/lib\(64\)\?\(32\)\?/ld@'$root'&@g' \
 		-e 's@/usr@'$root'/usr@g' $file.orig > $file
-#define STANDARD_STARTFILE_PREFIX_1 "'$root'/usr/lib64/"
-	echo '
+
+	#echo '
 #undef STANDARD_STARTFILE_PREFIX_1
 #undef STANDARD_STARTFILE_PREFIX_2
-#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib64/"
+#define STANDARD_STARTFILE_PREFIX_1 "'$root'/lib64/"
 #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
 	touch $file.orig
 done
